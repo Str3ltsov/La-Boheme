d43138c744b9650e474f079bca6c7c673011c6ec
Str3ltsov <andtaress@live.com>
2022-06-14 13:54:52 +0300

    Reservations now show error messages

--- resources/views/admin/reservations/messages.blade.php
+++ resources/views/admin/reservations/messages.blade.php
@@ -1,3 +1,12 @@
+@if($errors->any())
+    <div class="alert alert-danger">
+        <ul class="list-unstyled">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 @if (session()->has('success'))
     <div class="alert alert-success" id="message">
         {{ session()->get('success') }}
